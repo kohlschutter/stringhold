bd738fce54ed8f510de4b1985aea8b15627cc77d
Christian Kohlschütter <christian@kohlschutter.com>
2022-11-22 18:02:56 +0100

    Clarify ignorance about garbage-collected StringHolders

--- src/main/java/com/kohlschutter/stringhold/LimitedStringHolderScope.java
+++ src/main/java/com/kohlschutter/stringhold/LimitedStringHolderScope.java
@@ -17,12 +17,17 @@
  */
 package com.kohlschutter.stringhold;
 
+import java.lang.ref.WeakReference;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A {@link StringHolderScope} that keeps track of the aggregate minimum length of all associated
  * {@link StringHolder}s and, optionally, invokes a given callback whenever the limit is exceeded.
+ * 
+ * NOTE: This implementation does not keep track of {@link StringHolder}s being garbage-collected
+ * and thus removed implicitly from memory without triggering {@link #remove(StringHolder)}. If
+ * desired, you may want to implement a {@link WeakReference}-based wrapper.
  *
  * @author Christian Kohlschütter
  */
